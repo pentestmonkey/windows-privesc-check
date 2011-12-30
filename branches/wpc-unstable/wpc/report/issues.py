bfd8b7f490c3331027cb8ec27c1b7516133f48b5
mike.auty <mike.auty@06c163b4-3306-8fab-48da-fca92b9ad31a>
2011-12-30 21:02:11 +0000

    Add in first stab at outputting in an XML format.

--- branches/wpc-unstable/wpc/report/issues.py
+++ branches/wpc-unstable/wpc/report/issues.py
@@ -1,5 +1,7 @@
 from wpc.report.issue import issue
 
+import xml.etree.cElementTree as etree
+
 # TODO should this class contain info about the scan?  or define a new class called report?
 # Version of script
 # Date, time of audit
@@ -9,31 +11,30 @@ from wpc.report.issue import issue
 class issues:
     def __init__(self):
         self.issues = []
-        
+
     def get_by_id(self, id):
         # search for issue
         for i in self.issues:
             if i.get_id() == id:
                 return i
-        
+
         # create new issue
         i = issue(id)
         self.add_issue(i)
         return i
-        
+
     def add_issue(self, i):
         self.issues.append(i)
-    
+
     def add_supporting_data(self, id, k, v):
         self.get_by_id(id).add_supporting_data(k, v)
-    
+
     def get_all(self):
         return self.issues
-        
+
     def as_text(self):
-        t = '------------------------------------------------------------------'  + "\n"
+        r = etree.Element('report')
         for i in self.get_all():
-            t += i.as_text()
-        return t
-        
-    
+            r.append(i.as_text())
+        return etree.tostring(r)
+
