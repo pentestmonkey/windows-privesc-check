daa2858269e7389f5067677328357d2a35bf7a75
mike.auty <mike.auty@06c163b4-3306-8fab-48da-fca92b9ad31a>
2011-12-30 19:13:53 +0000

    PEP8: Convert all tabs to 4 spaces.

--- wpc/report/issues.py
+++ wpc/report/issues.py
@@ -7,33 +7,33 @@ from wpc.report.issue import issue
 # ...
 
 class issues:
-	def __init__(self):
-		self.issues = []
-		
-	def get_by_id(self, id):
-		# search for issue
-		for i in self.issues:
-			if i.get_id() == id:
-				return i
-		
-		# create new issue
-		i = issue(id)
-		self.add_issue(i)
-		return i
-		
-	def add_issue(self, i):
-		self.issues.append(i)
-	
-	def add_supporting_data(self, id, k, v):
-		self.get_by_id(id).add_supporting_data(k, v)
-	
-	def get_all(self):
-		return self.issues
-		
-	def as_text(self):
-		t = '------------------------------------------------------------------'  + "\n"
-		for i in self.get_all():
-			t += i.as_text()
-		return t
-		
-	
+    def __init__(self):
+        self.issues = []
+        
+    def get_by_id(self, id):
+        # search for issue
+        for i in self.issues:
+            if i.get_id() == id:
+                return i
+        
+        # create new issue
+        i = issue(id)
+        self.add_issue(i)
+        return i
+        
+    def add_issue(self, i):
+        self.issues.append(i)
+    
+    def add_supporting_data(self, id, k, v):
+        self.get_by_id(id).add_supporting_data(k, v)
+    
+    def get_all(self):
+        return self.issues
+        
+    def as_text(self):
+        t = '------------------------------------------------------------------'  + "\n"
+        for i in self.get_all():
+            t += i.as_text()
+        return t
+        
+    
